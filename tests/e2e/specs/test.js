3d9d9efc50472391cb2ffa8a0936c09270214b18
maron8676 <maron8676@gmail.com>
2020-01-03 23:41:26 +0900

    テスト例を追加した

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,8 +1,17 @@
 // https://docs.cypress.io/api/introduction/api.html
 
-describe("My First Test", () => {
-  it("Visits the app root url", () => {
-    cy.visit("/");
-    cy.contains("h1", "Welcome to Your Vue.js + TypeScript App");
+describe('My First Test', () => {
+  it('Visits the app root url', () => {
+    cy.visit('/');
+    cy.contains('h1', 'Welcome to Your Vue.js App');
+  });
+  it('Visits the app about url', () => {
+    cy.visit('/about');
+    cy.contains('h1', 'This is an about page');
+  });
+  it('Visits About after Home', () => {
+    cy.visit('/');
+    cy.get('[href="/about"]').click();
+    cy.contains('h1', 'This is an about page');
   });
 });
